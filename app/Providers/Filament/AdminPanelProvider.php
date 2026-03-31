1a48b066ed2e5f8b150c2d53ab85269ab71d26b1
Leandro <lean.ayala4@gmail.com>
2026-03-31 08:55:03 -0300

    Agregamos traducción y orden alfabetico 5

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,6 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->locale('es')
             ->colors([
                 'primary' => Color::Emerald,
             ])
