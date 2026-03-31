96cf6b9d386223f9ea12b342cc4f60d5a4df5eea
Leandro <lean.ayala4@gmail.com>
2026-03-31 08:30:59 -0300

    Agregamos logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,8 +28,12 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Emerald,
             ])
+            ->favicon(asset('images/logo-podologia.png'))
+            ->brandLogo(asset('images/logo-podologia.png'))
+            ->brandName('Podología - Balance')
+	        ->brandLogoHeight('60px')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
